dbc21642686734266c1bf6185cc23da49237072d
Nick van Dokkum <16072@ma-web.nl>
2015-10-19 19:44:32 +0200

    added start menu, and build the first prototype

--- Assets/Scripts/Misc/NextLevel.cs
+++ Assets/Scripts/Misc/NextLevel.cs
@@ -5,7 +5,8 @@ public class NextLevel : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Player") {
-            if (Application.loadedLevel < Application.levelCount) {
+            if (Application.loadedLevel < Application.levelCount - 1) {
+                PlayerPrefs.SetInt("Level", Application.loadedLevel + 1);
                 Application.LoadLevel(Application.loadedLevel + 1);
             }
             else {
---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour {
+
+    public Button continueButton;
+
+    void Start() {
+        if (PlayerPrefs.GetInt("Level") == 0) {
+            continueButton.interactable = false;
+        }
+    }
+    public void Continue() {
+        Application.LoadLevel(PlayerPrefs.GetInt("Level"));
+    }
+    public void NewGame() {
+        PlayerPrefs.SetInt("Level", 0);
+        Application.LoadLevel(1);
+    }
+}
